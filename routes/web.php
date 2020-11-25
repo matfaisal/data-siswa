3815e9133c1e4bf671bad6b041ac9599a080234a
Muhammad Faisal <muh.faisal572@gmail.com>
2020-11-26 02:35:53 +0700

    menambahkan fungsi login dan logout serta authentication

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,16 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/siswa', 'SiswaController@index');
-Route::post('/siswa/create', 'SiswaController@create');
-Route::get('/siswa/{id}/edit','SiswaController@edit');
-Route::post('/siswa/{id}/update','SiswaController@update');
-Route::get('/siswa/{id}/delete','SiswaController@delete');
+Route::get('/login', 'AuthController@login')->name('login');
+Route::post('/postlogin', 'AuthController@postlogin');
+Route::get('/logout', 'AuthController@logout');
+
+Route::get('/dashboard', 'DashboardController@index')->middleware('auth');
+Route::get('/siswa', 'SiswaController@index')->middleware('auth');
+Route::post('/siswa/create', 'SiswaController@create')->middleware('auth');
+Route::get('/siswa/{id}/edit','SiswaController@edit')->middleware('auth');
+Route::post('/siswa/{id}/update','SiswaController@update')->middleware('auth');
+Route::get('/siswa/{id}/delete','SiswaController@delete')->middleware('auth');
